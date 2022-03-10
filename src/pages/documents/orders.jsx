db618c510689e293a666715ea03603d5cda8cbea
geovannygil <geovannygr2018@gmail.com>
2022-03-10 00:45:38 -0600

    feat(Root): Implements views order, layout first form-general

--- src/pages/documents/orders.jsx
+++ src/pages/documents/orders.jsx
@@ -1,8 +1,92 @@
+
+import styled from 'styled-components'
+import { TitleMain, TitleLine } from '@/components/Elements/TItleMain'
+import { CardContent, CardHeader, CardTitle } from '@/components/Elements/Cards'
+import * as BoxIcon from 'react-icons/bi'
+import { CheckInput, InputElement, InputElementSelect, FormGroupAction } from '@/components/Elements/Inputs'
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 10px;
+  align-items: stretch;
+`
+
+const SectionCreateOrder = styled.section`
+  display: grid;
+  grid-gap: 1.5rem;
+  grid-template-columns: 1fr;
+
+  @media(min-width: 1280px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+
+const FormOrder = styled.div`
+  /* outline: 2px solid orange ; */
+`
+const CardTitleSelect = styled(CardTitle)`
+  color: var(--color-gray-light);
+`
+
+const SpanSelectClient = styled.span`
+  color: var(--color-white);
+  font-size: var(--smaller-font-size);
+  font-weight: var(--normal-font-size);
+  display: block;
+  width: 100%;
+  margin: 10px 0;
+`
+// const SectionClientTable = styled.div`
+//   /* outline: 2px solid orange ; */
+// `
+
 const Orders = () => {
   return (
-    <div className='title-page'>
-      <h1>Ordenes</h1>
-    </div>
+    <main>
+      <header className='Header__main-content'>
+        <TitleMain textNeutral='Crear' textColor=' Orden.' />
+      </header>
+      <SectionCreateOrder>
+        <FormOrder>
+          <FormGroupAction label='Cliente'>
+            <select>
+              <option value=''>Inbestgo</option>
+              <option value=''>Innovate</option>
+            </select>
+          </FormGroupAction>
+          <CardContent>
+            <CardHeader>
+              <CardTitleSelect>Cliente Seleccionado</CardTitleSelect>
+              <button className='Btn__icon'><BoxIcon.BiInfoCircle size='1.1em' /></button>
+            </CardHeader>
+            <div>
+              <SpanSelectClient>C-0000123</SpanSelectClient>
+              <SpanSelectClient>Angel Chajón</SpanSelectClient>
+            </div>
+          </CardContent>
+          <TitleLine>Generales</TitleLine>
+          <InputElement label='Fecha de orden'>
+            <input type='datetime-local' />
+          </InputElement>
+          <InputElementSelect label='Colaborador'>
+            <select>
+              <option value='1'>Geovanny Gil</option>
+              <option value='2'>Orlando Sul</option>
+            </select>
+          </InputElementSelect>
+          <InputElementSelect label='Estado de orden'>
+            <select>
+              <option value='1'>Cita Pendiente</option>
+              <option value='2'>Envio de cotización</option>
+            </select>
+          </InputElementSelect>
+          <CheckInput text='Agendar Cita'>
+            <input type='checkbox' />
+          </CheckInput>
+        </FormOrder>
+      </SectionCreateOrder>
+    </main>
   )
 }
 
