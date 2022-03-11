9bdcd5830aee84ae52ce5a41f82948cf91ff14f2
geovannygil <geovannygr2018@gmail.com>
2022-03-10 23:23:19 -0600

    chore(Root): Add vite config global to global this
    Install packages tiptap - rich-text, and config vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill'
 
 export default defineConfig({
 
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src')
     }
+  },
+  define: {
+    global: 'globalThis'
   }
 })
